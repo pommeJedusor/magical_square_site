f39be337ebb03af58a957b3fa9316507b8d82816
pommejedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-09-14 21:34:56 +0200

    pagination minimum page number

--- app/pagination.tsx
+++ app/pagination.tsx
@@ -9,7 +9,7 @@ export default function Pagination({ page_size, page_index }: { page_size: numbe
     <>
       <div className="flex flex-row justify-center mt-5">
         <button onClick={() => location.href = `./${target_page}`} className="mx-2 underline text-dark-white hover:text-white">Go to page {target_page} / {max_page}</button><br />
-        <input name='page_index' type='number' max={max_page} value={target_page} className="text-black mx-2 w-24" onChange={(event) => setTargetPage(Math.min(Number(event.target.value), max_page))} />
+        <input name='page_index' type='number' min="1" max={max_page} value={target_page} className="text-black mx-2 w-24" onChange={(event) => setTargetPage(Math.min(Number(event.target.value), max_page))} />
       </div>
     </>
   );
