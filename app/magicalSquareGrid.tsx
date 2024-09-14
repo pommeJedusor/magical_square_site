422cdb5cc18d13c00a7ca0d48fce0556be38729a
pommejedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-09-14 16:05:32 +0200

    hover

--- app/magicalSquareGrid.tsx
+++ app/magicalSquareGrid.tsx
@@ -3,30 +3,43 @@ import { useState } from "react";
 
 export default function MagicalSquareGrid() {
   const [grid, setGrid] = useState(Array.from({ length: 10 }, () => Array(10).fill(0)));
+  grid[0][0] = 1;
+  const [current_index, setCurrentIndex] = useState(2);
   const [isGameFinished, setGameFinished] = useState(false);
   return (
     <div className='w-full h-full bg-dark-white flex flex-col'>
       {grid.map((row, index) => (
-        <Row key={index} row={row} y={index} />
+        <Row key={index} grid={grid} y={index} current_index={current_index} />
       ))}
     </div>
   );
 }
 
-function Row({ row, y }: { row: Array<number>, y: number }) {
+function Row({ grid, y, current_index }: { grid: Array<Array<number>>, y: number, current_index: number }) {
   return (
     <div className={`w-full h-[10%] flex flex-row`}>
-      {row.map((square, index) => (
-        <Square square_value={square} x={index} y={y} />
+      {grid[y].map((square, index) => (
+        <Square grid={grid} x={index} y={y} current_index={current_index} />
       ))}
     </div>
   );
 }
 
-function Square({ square_value, x, y }: { square_value: number, x: number, y: number }) {
-  return (
-    <div className="w-[10%] h-full border text-dark-black flex items-center justify-center text-2xl">
-      {square_value ? square_value : null}
-    </div>
-  );
+function Square({ grid, x, y, current_index }: { grid: Array<Array<number>>, x: number, y: number, current_index: number }) {
+  const [isHovered, setHovered] = useState(false);
+  const square_value = grid[y][x];
+  if (!isHovered || square_value) {
+    return (
+      <div className="w-[10%] h-full border text-dark-black flex items-center justify-center text-2xl" onMouseEnter={() => setHovered(true)}>
+        {square_value ? square_value : null}
+      </div>
+    );
+  } else {
+    return (
+      <div className="w-[10%] h-full border text-dark-black/50 flex items-center justify-center text-2xl cursor-pointer" onMouseLeave={() => setHovered(false)}>
+        {square_value ? square_value : current_index}
+      </div>
+    );
+
+  }
 }
