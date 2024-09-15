fe07cff7fcdd53038b898f0905bae61f077d5ffb
pommejedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-09-15 10:57:18 +0200

    site's infos

---
+++ app/infos/page.tsx
@@ -0,0 +1,27 @@
+import { Metadata } from "next";
+import NavLayout from "../nav";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Site Info",
+  description: "Describe the site and gives interesting infos",
+};
+
+export default async function Page() {
+  return (
+    <div>
+      <NavLayout />
+      <h1 className="text-dark-white text-center text-4xl my-5">Informations</h1>
+      <h2 className="text-dark-white w-4/12 mx-auto text-3xl my-5">Solutions finder</h2>
+      <p className="w-4/12 mx-auto text-xl">
+        I coded a complex python algorithm that can find 33 938 944 solutions in only some minutes.
+        If recursives, transposition tables and binary operations don't scare you, here is <Link target="_blank" href="https://github.com/pommeJedusor/magical_square" className="text-dark-white hover:text-white hover:underline">the code</Link>
+      </p>
+      <h2 className="text-dark-white w-4/12 mx-auto text-3xl my-5">This site</h2>
+      <p className="w-4/12 mx-auto text-xl">
+        I coded this sites using react, next.js, typescript and tailwindcss.
+        if you want to see it, here is <Link target="_blank" href="https://github.com/pommeJedusor/magical_square_site" className="text-dark-white hover:text-white hover:underline"> the code</Link>
+      </p>
+    </div>
+  );
+}
--- app/nav.tsx
+++ app/nav.tsx
@@ -4,7 +4,7 @@ export default function NavLayout() {
   return (
     <div className="sticky top-0">
       <nav className="flex h-10 items-center bg-dark-black border-b">
-        {[{ "url": "/", "text": "Play" }, { "url": "/rules", "text": "Rules" }, { "url": "/get_page/10/1", "text": "See Solutions" }].map(link => (
+        {[{ "url": "/", "text": "Play" }, { "url": "/rules", "text": "Rules" }, { "url": "/get_page/10/1", "text": "See Solutions" }, { "url": "/infos", "text": "Site's infos" }].map(link => (
           <Link key={link.url} href={link.url} className="mx-5 text-xl text-light-grey hover:text-dark-white hover:underline">{link.text}</Link>
         ))}
       </nav>
