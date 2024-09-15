04be25c5c7cca5b43a85f99ba8f94c1d2bb18336
pommejedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-09-15 12:45:43 +0200

    trad navbar

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,10 +1,12 @@
+"use client"
 import Link from 'next/link'
 
 export default function NavLayout({ lang }: { lang: string }) {
+  const urls = lang == "fr" ? [{ "url": "/", "text": "Jouer" }, { "url": "/rules", "text": "Règles" }, { "url": "/get_page/10/1", "text": "Voir Les Solutions" }, { "url": "/infos", "text": "À Propos" }] : [{ "url": "/", "text": "Play" }, { "url": "/rules", "text": "Rules" }, { "url": "/get_page/10/1", "text": "See Solutions" }, { "url": "/infos", "text": "About" }];
   return (
     <div className="sticky top-0">
       <nav className="flex h-10 items-center bg-dark-black border-b">
-        {[{ "url": "/", "text": "Play" }, { "url": "/rules", "text": "Rules" }, { "url": "/get_page/10/1", "text": "See Solutions" }, { "url": "/infos", "text": "Site's infos" }].map(link => (
+        {urls.map(link => (
           <Link key={link.url} href={`/${lang}${link.url}`} className="mx-5 text-xl text-light-grey hover:text-dark-white hover:underline">{link.text}</Link>
         ))}
       </nav>
