7cceb2a081179d9b0a6b7da49c070c914f4dad7e
pommejedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-09-15 12:54:37 +0200

    infos

--- app/[lang]/infos/page.tsx
+++ app/[lang]/infos/page.tsx
@@ -8,20 +8,39 @@ export const metadata: Metadata = {
 };
 
 export default async function Page({ params }: { params: { lang: string } }) {
-  return (
-    <div>
-      <NavLayout lang={params.lang} />
-      <h1 className="text-dark-white text-center text-4xl my-5">Informations</h1>
-      <h2 className="text-dark-white w-4/12 mx-auto text-3xl my-5">Solutions finder</h2>
-      <p className="w-4/12 mx-auto text-xl">
-        I coded a complex python algorithm that can find 33 938 944 solutions in only some minutes.
-        If recursives, transposition tables and binary operations don't scare you, here is <Link target="_blank" href="https://github.com/pommeJedusor/magical_square" className="text-dark-white hover:text-white hover:underline">the code</Link>
-      </p>
-      <h2 className="text-dark-white w-4/12 mx-auto text-3xl my-5">This site</h2>
-      <p className="w-4/12 mx-auto text-xl">
-        I coded this sites using react, next.js, typescript and tailwindcss.
-        if you want to see it, here is <Link target="_blank" href="https://github.com/pommeJedusor/magical_square_site" className="text-dark-white hover:text-white hover:underline"> the code</Link>
-      </p>
-    </div>
-  );
+  if (params.lang == "fr") {
+    return (
+      <div>
+        <NavLayout lang={params.lang} />
+        <h1 className="text-dark-white text-center text-4xl my-5">Informations</h1>
+        <h2 className="text-dark-white w-4/12 mx-auto text-3xl my-5">Algorithme de résolution</h2>
+        <p className="w-4/12 mx-auto text-xl">
+          J'ai codé un algorithme complex en python qui peut trouver jusqu'à 33 938 944 solutions en seulement quelques minutes.
+          Si les récursives, les tables de transpositions et les opérations binaires ne vous font pas peur, voici <Link target="_blank" href="https://github.com/pommeJedusor/magical_square" className="text-dark-white hover:text-white hover:underline">le code</Link>
+        </p>
+        <h2 className="text-dark-white w-4/12 mx-auto text-3xl my-5">Le site</h2>
+        <p className="w-4/12 mx-auto text-xl">
+          J'ai codé ce site en utilisant react, next.js, typescript et tailwindcss.
+          si vous voulez le voir, voici <Link target="_blank" href="https://github.com/pommeJedusor/magical_square_site" className="text-dark-white hover:text-white hover:underline">le code</Link>
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <NavLayout lang={params.lang} />
+        <h1 className="text-dark-white text-center text-4xl my-5">Informations</h1>
+        <h2 className="text-dark-white w-4/12 mx-auto text-3xl my-5">Solutions finder</h2>
+        <p className="w-4/12 mx-auto text-xl">
+          I coded a complex python algorithm that can find 33 938 944 solutions in only some minutes.
+          If recursives, transposition tables and binary operations don't scare you, here is <Link target="_blank" href="https://github.com/pommeJedusor/magical_square" className="text-dark-white hover:text-white hover:underline">the code</Link>
+        </p>
+        <h2 className="text-dark-white w-4/12 mx-auto text-3xl my-5">This site</h2>
+        <p className="w-4/12 mx-auto text-xl">
+          I coded this site using react, next.js, typescript and tailwindcss.
+          if you want to see it, here is <Link target="_blank" href="https://github.com/pommeJedusor/magical_square_site" className="text-dark-white hover:text-white hover:underline">the code</Link>
+        </p>
+      </div>
+    );
+  }
 }
