1797fae8b3b60b943bf6f63ad01ca9a36a393c5f
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-09-16 11:53:26 +0200

    cancelCancel

--- app/userMagicalSquareGrid.tsx
+++ app/userMagicalSquareGrid.tsx
@@ -23,12 +23,24 @@ export default function UserMagicalSquareGrid() {
     setY(last_move.y);
   }
 
+  function cancelCancel() {
+    if (moves.current.children.length === 0) return;
+    let next_move = moves.current.children[moves.current.children.length - 1];
+    moves.addMove(next_move.x, next_move.y);
+
+    grid[next_move.y][next_move.x] = next_move.depth;
+    setGrid(grid.slice());
+    setCurrentDepth(moves.current.depth + 1);
+    setX(next_move.x);
+    setY(next_move.y);
+  }
+
 
   return (
     <div className='w-full h-full mx-auto my-5'>
       <div className="w-[90%] h-10% mx-auto">
         <button type="button" onClick={cancelMove} className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Cancel last move</button>
-        <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Cancel last cancel</button>
+        <button type="button" onClick={cancelCancel} className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Cancel last cancel</button>
       </div>
       <div className='w-[90%] h-[90%] mx-auto my-5'>
         <MagicalSquareGrid input_depth={current_depth} input_grid={grid} input_x={current_x} input_y={current_y} moves={moves} />
