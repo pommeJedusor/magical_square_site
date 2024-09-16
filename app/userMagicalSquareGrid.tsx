7d9b27ce823d035832a9dd7152fa335b4ff1feef
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-09-16 14:24:10 +0200

    improve button gui

--- app/userMagicalSquareGrid.tsx
+++ app/userMagicalSquareGrid.tsx
@@ -38,9 +38,13 @@ export default function UserMagicalSquareGrid() {
 
   return (
     <div className='w-full h-full mx-auto my-5'>
-      <div className="w-[90%] h-10% mx-auto">
-        <button type="button" onClick={cancelMove} className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Cancel last move</button>
-        <button type="button" onClick={cancelCancel} className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Cancel last cancel</button>
+      <div className="w-32 h-10% mx-auto">
+        <button onClick={cancelMove} type="button" className="mx-2 focus:outline-none focus:ring-4 focus:ring-green-300 dark:focus:ring-green-800 hover:outline-none hover:ring-4 hover:ring-green-300 dark:hover:ring-green-800 hover:bg-green-600 bg-green-700 p-1 rounded">
+          <img className="w-10" src="/arrow-u-up-left-svgrepo-com.svg" alt="" />
+        </button>
+        <button onClick={cancelCancel} type="button" className="mx-2 focus:outline-none focus:ring-4 focus:ring-red-300 dark:focus:ring-red-800 hover:outline-none hover:ring-4 hover:ring-red-300 dark:hover:ring-red-800 hover:bg-red-600 bg-red-700 p-1 rounded">
+          <img className="w-10" src="/arrow-u-up-right-svgrepo-com.svg" alt="" />
+        </button>
       </div>
       <div className='w-[90%] h-[90%] mx-auto my-5'>
         <MagicalSquareGrid input_depth={current_depth} input_grid={grid} input_x={current_x} input_y={current_y} moves={moves} />
