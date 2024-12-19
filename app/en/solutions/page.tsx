b0165e7e0f86ab4c5459db2bf34d64f9fca61858
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-12-19 19:53:31 +0100

    see solutions parallelize http requests

--- app/en/solutions/page.tsx
+++ app/en/solutions/page.tsx
@@ -5,7 +5,6 @@ import ArrowUp from '@/app/arrowUp';
 import MagicalSquareGrid from '@/components/magicalSquareGrid';
 import NavLayout from '@/components/nav';
 import Pagination from '@/components/pagination';
-import Link from 'next/link';
 
 class PageInfo{
     size: number;
@@ -34,19 +33,6 @@ function getGridFromPath(path: Array<number>): Grid {
   return new Grid(grid, 0, 0, 100, () => {});
 }
 
-async function fetchSolutions(page_size: string, page_index: string): Promise<Array<{ id: number, path: Array<number> }>> {
-  const min_index = (Number(page_index) - 1) * Number(page_size) + 1;
-  const max_index = (Number(page_index)) * Number(page_size);
-  const solutions = []
-  for (let i = min_index; i <= max_index; i++) {
-    const response = await fetch(`https://api_magical_square.chesspomme.com/get_path/${i}`);
-    const json = await response.json() as Array<number>;
-    const path = { id: i, path: json };
-    solutions.push(path);
-  }
-  return solutions;
-}
-
 class Grid {
   grid: number[][];
   x: number;
@@ -63,51 +49,27 @@ class Grid {
   }
 }
 
+function SolutionGrid({ page_index }: {page_index: number}){
+    const [path, setPath] = useState<Array<number>|null>(null);
+    useEffect(() => {
+        const getSolution = async () => {
+            const response = await fetch(`https://api_magical_square.chesspomme.com/get_path/${page_index}`);
+            const json = await response.json() as Array<number>;
+            setPath(json);
+        }
+        getSolution();
+    }, [page_index]);
+
+    return (
+        <>
+          <MagicalSquareGrid grid={path ? getGridFromPath(path) : new Grid(Array(10).fill(Array(10).fill(0)), 101, 0, 100, ()=>{})} input_moves={undefined} winning_moves={{ moves: [], must_show: false, reset_moves: () => { } }} />
+        </>
+    );
+}
+
 export default function Page() {
   const [page_info, setPageInfo] = useState(new PageInfo(10, 1, ()=>{}));
   page_info.setPageInfo = setPageInfo;
-
-  const [solutions, setSolutions] = useState<Array<{ id: number, path: Array<number> }> | null>(null);
-  const [errorMessage, setErrorMessage] = useState<string>('');
-
-  useEffect(() => {
-    const loadSolutions = async () => {
-      if (Number(page_info.size) > 100) {
-        setErrorMessage("You can't ask for more than 100 solutions by page");
-        return;
-      }
-
-      try {
-        const fetchedSolutions = await fetchSolutions(page_info.size.toString(), page_info.index.toString());
-        setSolutions(fetchedSolutions);
-      } catch (error) {
-        setErrorMessage("Didn't achieve to retrieve the data you were looking for :(");
-      }
-    };
-
-    loadSolutions();
-  }, [page_info]);
-
-  if (errorMessage) {
-    return (
-      <div>
-        <NavLayout lang={"en"} />
-        <h1 className='text-light-grey text-center text-2xl mt-5'>{errorMessage}</h1>
-        <p className='text-light-grey text-center text-2xl mt-5'>
-          Either come back to the previous url or go to <Link href="/">Home</Link>
-        </p>
-      </div>
-    );
-  }
-
-  if (!solutions) {
-    return (
-      <div>
-        <NavLayout lang={"en"} />
-        <h1 className='text-light-grey text-center text-2xl mt-5'>Loading...</h1>
-      </div>
-    );
-  }
 
   const renderContent = (lang: string) => {
     return (
@@ -118,12 +80,12 @@ export default function Page() {
         </h1>
         <Pagination page_info={page_info} lang={lang} />
         <div className='w-[97vw] flex flex-row flex-wrap justify-around my-5'>
-          {solutions.map((solution, index) => (
+          {Array(page_info.size).fill(1).map((solution, index) => (
             <div key={index} className='lg:w-[34vw] lg:h-[34vw] w-[68vw] h-[68vw] my-5'>
               <h3 className='text-dark-white mt-5 text-xl'>
                 {format_number(((page_info.index - 1) * page_info.size + index + 1).toString())}.
               </h3>
-              <MagicalSquareGrid key={solution.id} grid={getGridFromPath(solution.path)} input_moves={undefined} winning_moves={{ moves: [], must_show: false, reset_moves: () => { } }} />
+              <SolutionGrid key={index} page_index={(page_info.index - 1) * page_info.size + index} />
             </div>
           ))}
         </div>
